2e8d40c40a156fcc4a570e1c2bd27e8037015d93
gfhorigin <98154676+gfhorigin@users.noreply.github.com>
2025-07-04 12:34:01 +0700

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,10 @@ db.add_name('admin')
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['http://localhost:5173']
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 @app.post("/login")

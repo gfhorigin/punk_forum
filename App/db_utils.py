9cfc636603c54e1c402dab26487f245aaf26ca92
gfhorigin <98154676+gfhorigin@users.noreply.github.com>
2025-07-06 17:16:11 +0700

    refactor repository

--- App/db_utils.py
+++ App/db_utils.py
@@ -1,4 +1,4 @@
-import mysql.connector
+#import mysql.connector
 import os
 from dotenv import load_dotenv
 #punk_forum_db, username root, 127.0.0.1, port 3306
@@ -6,12 +6,12 @@ class DB:
     def __init__(self):
         load_dotenv()
 
-        db = mysql.connector.connect(
-            host="mysql",
-            user="root",
-            passwd=os.getenv('DB_PASSWD'),
-            database="punk_forum_db"
-        )
+        # db = mysql.connector.connect(
+        #     host="mysql",
+        #     user="root",
+        #     passwd=os.getenv('DB_PASSWD'),
+        #     database="punk_forum_db"
+        # )
         self.users = []
         self.users_name = []
 

9a90f85f9a06385667c1f5cabb6eb5b09593af0a
gfhorigin <98154676+gfhorigin@users.noreply.github.com>
2025-07-15 01:47:35 +0700

    fix and add auth info

--- db_utils.py
+++ db_utils.py
@@ -16,11 +16,11 @@ class DB:
         self.cursor = self.db.cursor(buffered=True)
 
     def add_user(self, login_hash, email, nickname, unique_name):
-        current_time = datetime.datetime.now()
+        current_time = str(datetime.datetime.now())
 
         self.cursor.execute(f'INSERT INTO {config.TABLE_USERS} '
                             f'(unique_name, nickname,email, login_hash, last_login, created_at, updated_at )'
-                            f'VALUES(%s, %s, %s, %s, %s, %s)',
+                            f'VALUES(%s, %s, %s, %s, %s, %s,%s)',
                             (unique_name, nickname, email, login_hash, current_time, current_time, current_time))
 
         self.db.commit()
@@ -29,25 +29,41 @@ class DB:
         return self.users
 
     def get_emails(self):  # ------------возможно в будущем удалить
-        return self.cursor.execute(f'SELECT email FROM {config.TABLE_USERS}').fetchall()
+        self.cursor.execute(f'SELECT email FROM {config.TABLE_USERS}')
+        return self.cursor.fetchall()
 
     def user_info(self, name):
         return {"userinfo": 'info from database'}
 
     def get_unique(self):  # ------------возможно в будущем удалить
-        return self.cursor.execute(f'SELECT unique_name FROM {config.TABLE_USERS}').fetchall()
+        self.cursor.execute(f'SELECT unique_name FROM {config.TABLE_USERS}')
+        return self.cursor.fetchall()
 
     def email_exists(self, email):
-        if self.cursor.execute(f'SELECT email FROM {config.TABLE_USERS} WHERE email = %s', (email,)).fetchone():
+        self.cursor.execute(f'SELECT email FROM {config.TABLE_USERS} WHERE email = %s', (email,))
+        if self.cursor.fetchone():
             return True
         return False
 
     def unique_exists(self, unique):
-        if self.cursor.execute(f'SELECT unique_name FROM {config.TABLE_USERS} WHERE unique_name = %s', (unique,)).fetchone():
+        self.cursor.execute(f'SELECT unique_name FROM {config.TABLE_USERS} WHERE unique_name = %s', (unique,))
+        if self.cursor.fetchone():
             return True
         return False
 
     def hash_check(self, login_hash):
-        if self.cursor.execute(f'SELECT login_hash FROM {config.TABLE_USERS} WHERE login_hash = %s', (login_hash,)).fetchone():
+
+        self.cursor.execute(f'SELECT login_hash FROM {config.TABLE_USERS} WHERE login_hash = %s', (login_hash,))
+        if self.cursor.fetchone() :
             return True
         return False
+
+    def get_user_auth_info_by_email(self, email):
+        self.cursor.execute(f'SELECT unique_name, nickname, email, id  FROM {config.TABLE_USERS} WHERE email = %s',
+                            (email,))
+        return self.cursor.fetchone()
+
+    def delete_user(self, email): # --------------только для тестов
+        self.cursor.execute(f'DELETE FROM {config.TABLE_USERS} WHERE email = %s', (email,))
+        self.db.commit()
+

63f43dc0971807dcbb6c3efec468cc850c4bffc4
gfhorigin <98154676+gfhorigin@users.noreply.github.com>
2025-07-14 20:54:07 +0700

    update config

--- config/open_string_config.py
+++ config/open_string_config.py
@@ -1,6 +1,6 @@
 # names
 COOKIE_NAME = "token"
-
+TABLE_USERS = "users"
 
 # error types
 INVALID_AUTH = "cookie not allowed"
@@ -12,3 +12,6 @@ SUCCESS_BASE = "successfully"
 
 # messages
 M_INVALID_AUTH = "пользователь не прошел аунтефикацию"
+M_INVALID_CREDENTIALS = "неверынй логин или пароль"
+M_EMAIL_EXISTS = "такой пользователь уже существует"
+M_EMAIL_UNIQUE = "данное имя уже занято"
